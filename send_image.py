e6d3aa8a6a254bcd995f4f83bc7d5a3ce184384d
Vegirowthu <vamsi@profintech.in>
2026-03-05 22:26:11 +0530

    add python files

---
+++ send_image.py
@@ -0,0 +1,62 @@
+import os
+from twilio.rest import Client
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+client = Client(account_sid, auth_token)
+
+# 3 image URLs
+images = {
+    "1": "https://demo.twilio.com/owl.png",
+    "2": "https://images.unsplash.com/photo-1517849845537-4d257902454a",  # dog
+    "3": "https://images.unsplash.com/photo-1546182990-dffeafbe841d"       # lion
+}
+
+# Select image
+choice = input("Select image (1 / 2 / 3): ")
+image_url = images[choice]
+
+# Multiple phone numbers
+phone_numbers = [
+    "+917032455410",
+    "+918501830360"
+]
+
+# Schedule time (5:43 PM = 17:43)
+schedule_time = "17:43"
+
+print("Waiting for scheduled time...")
+
+while True:
+    current_time = datetime.now().strftime("%H:%M")
+
+    if current_time == schedule_time:
+
+        for phone in phone_numbers:
+            message = client.messages.create(
+                from_='whatsapp:+14155238886',
+                media_url=[image_url],
+                body="Scheduled Reminder",
+                to='whatsapp:' + phone
+            )
+
+            print("Sent to:", phone)
+
+        break
+
+    time.sleep(20)
+
+
+
+    
+
+
+
+
+
+    
+
+
+ #account_sid = "AC957e53546b73590ff8db2a02087bb9c1"
+#auth_token = "fa6f5b80429ccab5064237bb402cfb0f"
